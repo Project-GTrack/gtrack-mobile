2f6f171b9a622b93018e3934814780e7e6d4ca6c
Rj Oliverio <rjoliverio18@gmail.com>
2022-01-04 15:37:52 +0800

    schedule, profile, report pages

--- components/pages/SchedulePage.js
+++ components/pages/SchedulePage.js
@@ -1,42 +1,88 @@
 import React from 'react'
 import {
     Text,
-    Image,
-    Button,
     Center,
-    Input,
+    VStack,
+    HStack,
+    Container,
     Divider,
-    Link,
-    Box,
-    Stack
+    Icon,
+    Box
   } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons"
-import GtrackMainLogo from '../../assets/gtrack-logo-1.png'
-import GoogleIcon from '../../assets/google-icon.png'
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { MaterialIcons } from "@expo/vector-icons";
 
 
 const SchedulePage = () => {
     return (
-        <>
         <Center
-            px={3}
-            mt={10}
         >
-            <Image
-                size={200}
-                resizeMode={"contain"}
-                source={GtrackMainLogo}
-                alt="GTrack Logo"
-            />
-            <Text
-                mb={5}
-                fontSize={"xl"}
-                color={"gray.600"}
+            <Box
+                style={{
+                    alignSelf:'stretch',
+                    height:'100%'
+                }}
             >
-                Schedules
-            </Text>
+                <Calendar 
+                    style={{
+                        alignSelf:'stretch',
+                    }}
+                />
+                <Box 
+                    bg={'gray.100'}
+                    style={{
+                        alignSelf:'stretch',
+                        height:'100%'
+                    }}
+                >
+                    <VStack p={4} space={3}>
+                        <Text
+                            style={{
+                                fontSize:17
+                            }}
+                        >
+                            Tuesday, January 4, 2022
+                        </Text>
+                        <Divider
+                            style={{
+                                alignSelf:'stretch',
+                            }}
+                            bg={'gray.500'}
+                        />
+                        <HStack space={3}>
+                            <Icon
+                                as={<MaterialIcons name="info" />}
+                                size={25}
+                                color={'#10b981'}
+                            />
+                            <Text>
+                                [Poblacion] Garbage Collection Day
+                            </Text>
+                        </HStack>
+                        <HStack space={3}>
+                            <Icon
+                                as={<MaterialIcons name="schedule" />}
+                                size={25}
+                                color={'#10b981'}
+                            />
+                            <Text>
+                                All day
+                            </Text>
+                        </HStack>
+                        <HStack space={3}>
+                            <Icon
+                                as={<MaterialIcons name="notes" />}
+                                size={25}
+                                color={'#10b981'}
+                            />
+                            <Text>
+                                Biodegradable waste only
+                            </Text>
+                        </HStack>
+                    </VStack>
+                </Box>
+            </Box>
         </Center>
-        </>
     )
 }
 
