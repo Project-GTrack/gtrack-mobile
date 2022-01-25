c00f574394c2003b5eca70e256b951c0833500d7
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2022-01-25 11:15:21 +0800

    Added Marker on Driver Track Collector Page

--- components/pages/driver/TrackCollectorPage.js
+++ components/pages/driver/TrackCollectorPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import {
     Text,
     Image,
@@ -13,11 +13,39 @@ import {
 import { MaterialIcons } from "@expo/vector-icons"
 import GtrackMainLogo from '../../../assets/gtrack-logo-1.png'
 import GoogleIcon from '../../../assets/google-icon.png'
-import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import MapView,{Marker} from 'react-native-maps';
 import { Dimensions } from 'react-native';
 
 
 const TrackCollectorPage = () => {
+    const [initLoc,setInitLoc]=useState({
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421})
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                Linking.openURL('app-settings:');
+              return;
+            }
+            let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.BestForNavigation, maximumAge: 10000});
+            console.log(location);
+            setInitLoc(prevState=>({...prevState,latitude:location.coords.latitude,longitude:location.coords.longitude}))
+            // Geolocation.getCurrentPosition(
+            //     (position) => {
+            //       setInitLoc(prevState=>({...prevState,latitude:position.coords.latitude,longitude:position.coords.longitude}))
+            //     },
+            //     (error) => {
+            //       // See error code charts below.
+            //       console.log(error.code, error.message);
+            //     },
+            //     { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+            // );
+          })();
+    }, [])
     return (
         <>
         <Center
@@ -25,10 +53,16 @@ const TrackCollectorPage = () => {
             alignSelf:'stretch'
         }}
         >
-            <MapView style={{
+            <MapView
+            region={initLoc}
+            style={{
                 width: Dimensions.get('window').width,
                 height: Dimensions.get('window').height,
-            }} />
+            }} >
+            <Marker
+                coordinate={{ latitude : initLoc.latitude , longitude : initLoc.longitude }}
+                />
+            </MapView>
         </Center>
         </>
     )
