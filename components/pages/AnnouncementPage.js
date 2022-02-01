a1f862681384004bff5bc53a7821c923719cff03
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2022-02-01 20:08:51 +0800

    Updated AnnouncementPage

--- components/pages/AnnouncementPage.js
+++ components/pages/AnnouncementPage.js
@@ -33,7 +33,7 @@ const AnnouncementPage = () => {
   const [data,setData]=useState([]);
   let temp = [];
   useEffect(()=>{
-    axios.get("https://white-ladybug-25.loca.lt/mobile/announcement/get-announcements")
+    axios.get(`${envs.BACKEND_URL}/mobile/announcement/get-announcements`)
       .then((res) => {
         temp=res.data.data;
         setInfo(temp);
